d19427b816a7bf5d8493f9b888923934750aa7c7
zinja-coder <jafarpathan9090@gmail.com>
2026-01-25 00:32:44 +0530

    release: v6.1.0

--- src/main/java/com/zin/jadxaimcp/utils/JadxAIMCPBanner.java
+++ src/main/java/com/zin/jadxaimcp/utils/JadxAIMCPBanner.java
@@ -13,5 +13,5 @@ public class JadxAIMCPBanner {
             "\n\n"+
             "\nAuthor         -> Jafar Pathan (zinja-coder@github)"+
             "\nFor Issues     -> https://github.com/zinja-coder/jadx-ai-mcp"+
-            "\nPlugin Version -> v6.0.0";
+            "\nPlugin Version -> v6.1.0";
 }
